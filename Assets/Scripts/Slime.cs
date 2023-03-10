b9a599fc5ff653c086c719e330558bf2130ee76f
Dalton <daltonstwilliams@gmail.com>
2023-03-09 21:42:09 -0600

    Big slime make little slime

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -1,19 +1,27 @@
+using System;
 using Godot;
 
 namespace Cowball
 {
-    public partial class Bullet : Area2D
+    public partial class Slime : Area2D
     {
+        private Random _random;
+
+        private Sprite2D _sprite;
 
         [Export] public float Damage = 7.5F;
 
         [Export] private int _speed = 700;
         [Export] private int _spread = 5;
-        [Export] private int _distanceAllowed = 400;
+        [Export] private int _distanceAllowed = 500;
         [Export] private double _distanceTravelled;
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
+            _sprite = GetNode<Sprite2D>("Sprite");
+
+            _random = new Random();
+            RotationDegrees += _random.Next(-15, 15);
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,16 +32,13 @@ namespace Cowball
 
             Position += movement;
             _distanceTravelled += _speed * delta;
+            _sprite.GlobalRotation = 0;
             if (_distanceTravelled > _distanceAllowed)
             {
-                FreeBullet();
+                QueueFree();
             }
         }
 
-        private void FreeBullet()
-        {
-            QueueFree();
-        }
+
     }
 }
-
--- Assets/Scripts/SlimeBoss.cs
+++ Assets/Scripts/SlimeBoss.cs
@@ -2,71 +2,77 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public partial class SlimeBoss : CharacterBody2D
+namespace Cowball
 {
-    private const string IdleAnim = "idle";
-    private const string SquishAnim = "squish";
-    private const string JumpAnim = "jump";
-
-    private Random _random;
-
-    private AnimatedSprite2D _sprite;
-    [Export] public int MinIdleLoopsBeforeJump = 4;
-    private int _idleLoopsCompleted;
-    [Export] public int PercentChanceJump = 80;
-    private bool _isJumping;
-    private bool _isFacingLeft;
-    private bool _canFlip;
-
-    // Jump properties
-    [Export] public float JumpHeight = 120; //pixels
-    [Export] public float TimeInAir = 0.18F; //honestly no idea
-    [Export] public float JumpSpeedHoriz = 350F;
-    [Export] public float JumpSpeed;
-    [Export] public float Gravity;
-
-    // Collisions
-    private CollisionPolygon2D _currentCollision;
-    private CollisionPolygon2D[] _idleCollisions;
-    private CollisionPolygon2D[] _squishCollisions;
-    private CollisionPolygon2D[] _jumpCollisions;
-
-    public override void _Ready()
+    public partial class SlimeBoss : CharacterBody2D
     {
-        _sprite = GetNode<AnimatedSprite2D>("Sprite");
-        _sprite.Play(IdleAnim);
+        private const string IdleAnim = "idle";
+        private const string SquishAnim = "squish";
+        private const string JumpAnim = "jump";
 
-        _random = new Random();
+        private Random _random;
 
-        // Calculations from https://medium.com/@brazmogu/physics-for-game-dev-a-platformer-physics-cheatsheet-f34b09064558
-        Gravity = (float)(JumpHeight / (2 * Math.Pow(TimeInAir, 2)));
-        JumpSpeed = (float)Math.Sqrt(2 * JumpHeight * Gravity);
+        private PackedScene _slimeScene;
 
-        LoadCollisions();
+        private AnimatedSprite2D _sprite;
+        [Export] public int MinIdleLoopsBeforeJump = 4;
+        private int _idleLoopsCompleted;
+        [Export] public int PercentChanceJump = 80;
+        private bool _isJumping;
+        private bool _isFacingLeft;
+        private bool _canFlip;
 
-    }
+        // Jump properties
+        [Export] public float JumpHeight = 120; //pixels
+        [Export] public float TimeInAir = 0.18F; //honestly no idea
+        [Export] public float JumpSpeedHoriz = 350F;
+        [Export] public float JumpSpeed;
+        [Export] public float Gravity;
 
-    private void LoadCollisions()
-    {
-        var allZero = GetNode<CollisionPolygon2D>("allZero");
-        var idleOneFive = GetNode<CollisionPolygon2D>("idleOneFive");
-        var idleTwoFour = GetNode<CollisionPolygon2D>("idleTwoFour");
-        var idleThree = GetNode<CollisionPolygon2D>("idleThree");
-        var jumpOne = GetNode<CollisionPolygon2D>("jumpOne");
-        var jumpTwo = GetNode<CollisionPolygon2D>("jumpTwo");
-        var jumpThree = GetNode<CollisionPolygon2D>("jumpThree");
-        var jumpFour = GetNode<CollisionPolygon2D>("jumpFour");
-        var squishOneEleven = GetNode<CollisionPolygon2D>("squishOneEleven");
-        var squishTwoTen = GetNode<CollisionPolygon2D>("squishTwoTen");
-        var squishThreeNine = GetNode<CollisionPolygon2D>("squishThreeNine");
-        var squishFourEight = GetNode<CollisionPolygon2D>("squishFourEight");
-        var squishFiveSeven = GetNode<CollisionPolygon2D>("squishFiveSeven");
-        var squishSix = GetNode<CollisionPolygon2D>("squishSix");
+        // Collisions
+        private CollisionPolygon2D _currentCollision;
+        private CollisionPolygon2D[] _idleCollisions;
+        private CollisionPolygon2D[] _squishCollisions;
+        private CollisionPolygon2D[] _jumpCollisions;
 
-        _currentCollision = allZero;
-
-        CollisionPolygon2D[] idle =
+        public override void _Ready()
         {
+            _slimeScene = GD.Load<PackedScene>("res://Assets/Scenes/Enemies/Slime.tscn");
+
+            _sprite = GetNode<AnimatedSprite2D>("Sprite");
+            _sprite.Play(IdleAnim);
+
+            _random = new Random();
+
+            // Calculations from https://medium.com/@brazmogu/physics-for-game-dev-a-platformer-physics-cheatsheet-f34b09064558
+            Gravity = (float)(JumpHeight / (2 * Math.Pow(TimeInAir, 2)));
+            JumpSpeed = (float)Math.Sqrt(2 * JumpHeight * Gravity);
+
+            LoadCollisions();
+
+        }
+
+        private void LoadCollisions()
+        {
+            var allZero = GetNode<CollisionPolygon2D>("allZero");
+            var idleOneFive = GetNode<CollisionPolygon2D>("idleOneFive");
+            var idleTwoFour = GetNode<CollisionPolygon2D>("idleTwoFour");
+            var idleThree = GetNode<CollisionPolygon2D>("idleThree");
+            var jumpOne = GetNode<CollisionPolygon2D>("jumpOne");
+            var jumpTwo = GetNode<CollisionPolygon2D>("jumpTwo");
+            var jumpThree = GetNode<CollisionPolygon2D>("jumpThree");
+            var jumpFour = GetNode<CollisionPolygon2D>("jumpFour");
+            var squishOneEleven = GetNode<CollisionPolygon2D>("squishOneEleven");
+            var squishTwoTen = GetNode<CollisionPolygon2D>("squishTwoTen");
+            var squishThreeNine = GetNode<CollisionPolygon2D>("squishThreeNine");
+            var squishFourEight = GetNode<CollisionPolygon2D>("squishFourEight");
+            var squishFiveSeven = GetNode<CollisionPolygon2D>("squishFiveSeven");
+            var squishSix = GetNode<CollisionPolygon2D>("squishSix");
+
+            _currentCollision = allZero;
+
+            CollisionPolygon2D[] idle =
+            {
             allZero,
             idleOneFive,
             idleTwoFour,
@@ -75,8 +81,8 @@ public partial class SlimeBoss : CharacterBody2D
             idleOneFive
         };
 
-        CollisionPolygon2D[] jump =
-        {
+            CollisionPolygon2D[] jump =
+            {
             allZero,
             jumpOne,
             jumpTwo,
@@ -84,8 +90,8 @@ public partial class SlimeBoss : CharacterBody2D
             jumpFour
         };
 
-        CollisionPolygon2D[] squish =
-        {
+            CollisionPolygon2D[] squish =
+            {
             allZero,
             squishOneEleven,
             squishTwoTen,
@@ -100,132 +106,146 @@ public partial class SlimeBoss : CharacterBody2D
             squishOneEleven
         };
 
-        CollisionPolygon2D[][] allCollisions = {
+            CollisionPolygon2D[][] allCollisions = {
             idle,
             jump,
             squish
         };
 
-        _idleCollisions = idle;
-        _jumpCollisions = jump;
-        _squishCollisions = squish;
+            _idleCollisions = idle;
+            _jumpCollisions = jump;
+            _squishCollisions = squish;
 
-        foreach (var list in allCollisions)
-        {
-            foreach (var col in list)
+            foreach (var list in allCollisions)
             {
-                col.Disabled = true;
+                foreach (var col in list)
+                {
+                    col.Disabled = true;
+                }
             }
         }
 
-    }
-
-    public override void _Process(double delta)
-    {
-        if (!_sprite.IsPlaying()) return;
-
-        _currentCollision.Disabled = true;
-        var frame = _sprite.Frame;
-        // ReSharper disable once ConvertSwitchStatementToSwitchExpression
-        switch (_sprite.Animation)
+        public override void _Process(double delta)
         {
-            case IdleAnim:
-                _currentCollision = _idleCollisions[frame];
-                break;
+            if (!_sprite.IsPlaying()) return;
 
-            case JumpAnim:
-                _currentCollision = _jumpCollisions[frame];
-                break;
-
-            case SquishAnim:
-                _currentCollision = _squishCollisions[frame];
-                break;
-        }
-
-        _currentCollision.Disabled = false;
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        var velocity = Velocity;
-        velocity.Y += Gravity * (float)delta;
-
-        // Add the gravity.
-        if (!IsOnFloor())
-        {
-            if (IsOnWall() && _canFlip)
+            _currentCollision.Disabled = true;
+            var frame = _sprite.Frame;
+            // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+            switch (_sprite.Animation)
             {
-                GD.Print("WALL");
-                velocity.X *= -1;
-                _canFlip = false;
-                _isFacingLeft = !_isFacingLeft;
-                _sprite.FlipH = !_sprite.FlipH;
+                case IdleAnim:
+                    _currentCollision = _idleCollisions[frame];
+                    break;
+
+                case JumpAnim:
+                    _currentCollision = _jumpCollisions[frame];
+                    break;
+
+                case SquishAnim:
+                    _currentCollision = _squishCollisions[frame];
+                    break;
             }
+
+            _currentCollision.Disabled = false;
         }
 
-        else
+        public override void _PhysicsProcess(double delta)
         {
-            if (_isJumping)
+            var velocity = Velocity;
+            velocity.Y += Gravity * (float)delta;
+
+            // Add the gravity.
+            if (!IsOnFloor())
             {
-                velocity.X = 0;
-                _isJumping = false;
-                _sprite.Play(IdleAnim);
+                if (IsOnWall() && _canFlip)
+                {
+                    GD.Print("WALL");
+                    velocity.X *= -1;
+                    _canFlip = false;
+                    _isFacingLeft = !_isFacingLeft;
+                    _sprite.FlipH = !_sprite.FlipH;
+                }
+            }
+
+            else
+            {
+                if (_isJumping)
+                {
+                    velocity.X = 0;
+                    _isJumping = false;
+                    _sprite.Play(IdleAnim);
+                    _idleLoopsCompleted = 0;
+                }
+
+            }
+
+            if (_sprite.Animation == IdleAnim) // if idle anim...
+            {
+                if (!_sprite.IsPlaying()) // check if anim is done. If yes...
+                {
+                    if (_idleLoopsCompleted >= MinIdleLoopsBeforeJump) //check if "min idle loops has passed". If yes...
+                    {
+                        if (_random.Next(0, 100) < PercentChanceJump + _idleLoopsCompleted - MinIdleLoopsBeforeJump)
+                        {
+                            _idleLoopsCompleted = 0;
+                            _sprite.Play(SquishAnim);
+                        }
+
+                        else
+                        {
+                            _idleLoopsCompleted = 0;
+                            _sprite.Play(IdleAnim);
+                        }
+                    }
+
+                    else // if not, increment loopsDone, start idle again
+                    {
+                        _idleLoopsCompleted += 1;
+                        _sprite.Play(IdleAnim);
+                        GD.Print("loop!");
+                    }
+                }
+            }
+
+            else if (_sprite.Animation == SquishAnim && !_sprite.IsPlaying())
+            {
+                _sprite.Play(JumpAnim);
                 _idleLoopsCompleted = 0;
             }
 
+            else if (_sprite.Animation == JumpAnim && !_sprite.IsPlaying() && !_isJumping)
+            {
+                velocity = Jump(velocity);
+                Shoot();
+            }
+
+            Velocity = velocity;
+            MoveAndSlide();
         }
 
-        if (_sprite.Animation == IdleAnim) // if idle anim...
+        private Vector2 Jump(Vector2 velocity)
         {
-            if (!_sprite.IsPlaying()) // check if anim is done. If yes...
+            _isJumping = true;
+            _canFlip = true;
+            var dir = _isFacingLeft ? -1 : 1;
+            return new Vector2(velocity.X + JumpSpeedHoriz * dir, velocity.Y - JumpSpeed);
+
+            // Shoot out projectiles in a circle around you
+            // Maybe shoot projectiles when you land as well, as a difficulty setting?
+
+        }
+
+        private void Shoot()
+        {
+            foreach (Marker2D point in GetTree().GetNodesInGroup("slimeSpawns"))
             {
-                if (_idleLoopsCompleted >= MinIdleLoopsBeforeJump) //check if "min idle loops has passed". If yes...
-                {
-                    if (_random.Next(0, 100) < PercentChanceJump + _idleLoopsCompleted - MinIdleLoopsBeforeJump)
-                    {
-                        _idleLoopsCompleted = 0;
-                        _sprite.Play(SquishAnim);
-                    }
-
-                    else
-                    {
-                        _idleLoopsCompleted = 0;
-                        _sprite.Play(IdleAnim);
-                    }
-                }
-
-                else // if not, increment loopsDone, start idle again
-                {
-                    _idleLoopsCompleted += 1;
-                    _sprite.Play(IdleAnim);
-                    GD.Print("loop!");
-                }
+                var newSlime = (Slime)_slimeScene.Instantiate();
+                newSlime.GlobalPosition = point.GlobalPosition;
+                newSlime.GlobalRotation = point.GlobalRotation;
+                //newSlime.SetRotation(Rotation);
+                GetParent().AddChild(newSlime);
             }
         }
-
-        else if (_sprite.Animation == SquishAnim && !_sprite.IsPlaying())
-        {
-            _sprite.Play(JumpAnim);
-            _idleLoopsCompleted = 0;
-        }
-
-        else if (_sprite.Animation == JumpAnim && !_sprite.IsPlaying() && !_isJumping)
-        {
-            velocity = Jump(velocity);
-        }
-
-        Velocity = velocity;
-        MoveAndSlide();
-    }
-
-    private Vector2 Jump(Vector2 velocity)
-    {
-        _isJumping = true;
-        _canFlip = true;
-        var dir = _isFacingLeft ? -1 : 1;
-        return new Vector2(velocity.X + JumpSpeedHoriz * dir, velocity.Y - JumpSpeed);
-
-        // Shoot out projectiles in a circle around you
-        // Maybe shoot projectiles when you land as well, as a difficulty setting?
     }
 }
